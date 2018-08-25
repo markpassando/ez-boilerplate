4a6b810897452bf5b2767a28ff5ef4c59d0f15ce
MarkPassando <markpassando@gmail.com>
2018-08-25 18:05:23 -0400

    Add skeleton for server side rendering

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>Fooo</div>
+      <button onClick={() => console.log('foo')} >FOO</button>
+    </div>
+  );
+};
+
+export default Home;
